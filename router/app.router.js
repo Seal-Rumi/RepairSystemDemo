187d6dbe05b10e3a96ea0686733318ba907d3f5a
Seal-Rumi <sealsaby1201@gmail.com>
2024-12-17 13:45:49 +0800

    seal

--- router/app.router.js
+++ router/app.router.js
@@ -6,49 +6,6 @@ router.get("/menu", function (req, res) {
 router.get("/", function (req, res) {
   res.send({ content: "This is app router" });
 });
-router.get("/menu_old", function (req, res) {
-  res.render("menu_old.html", {});
-});
-router.get("/menu", function (req, res) {
-  res.render("menu.html", {});
-});
-router.get("/menu2", function (req, res) {
-  res.render("menu2.html", {});
-});
-router.get("/manage_model", function (req, res) {
-  res.render("manage_model.html", {});
-});
-router.get("/manage_model_v2", function (req, res) {
-  res.render("manage_model_v2.html", {});
-});
-router.get("/model_show_view", function (req, res) {
-  res.render("model_show_view.html", {});
-});
-
-// 帳戶管理系統
-router.get("/system/accManage", function (req, res) {
-  res.render("system/accManage.html", {});
-});
-router.get("/system/sysAnnc", function (req, res) {
-  res.render("system/sysAnnc.html", {});
-});
-router.get("/system/staffManage", function (req, res) {
-  res.render("system/staffManage.html", {});
-});
-router.get("/system/verCtrl", function (req, res) {
-  res.render("system/verCtrl.html", {});
-});
-
-// 資產管理系統
-router.get("/asset/mediaRepo", function (req, res) {
-  res.render("asset/mediaRepo.html", {});
-});
-router.get("/asset/logisticsDist", function (req, res) {
-  res.render("asset/logisticsDist.html", {});
-});
-router.get("/asset/deviceManage", function (req, res) {
-  res.render("asset/deviceManage.html", {});
-});
 
 // 維護管理系統
 router.get("/repair/repairManage", function (req, res) {
--- router/file.router.js
+++ router/file.router.js
@@ -11,164 +11,25 @@ connectInfo = argv.dev ? connectInfo.dev : connectInfo.prod;
 let baseURL = {
   to_db_api: `http://${connectInfo.db.host}:${connectInfo.db.port}`,
 };
-let outputFileName = "";
-const storage_image = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "media/mediaRepo/img"); // 儲存的目錄
-  },
-  filename: function (req, file, cb) {
-    const originalExt = path.extname(file.originalname);
-    const newFileName = `img_${new Date().getTime()}${originalExt}`;
-    outputFileName = newFileName;
-    cb(null, newFileName);
-  },
-});
-const upload_image = multer({ storage: storage_image });
 
-const storage_model = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "media/mediaRepo/model"); // 儲存的目錄
-  },
-  filename: function (req, file, cb) {
-    const originalExt = path.extname(file.originalname);
-    const newFileName = `glb_${new Date().getTime()}${originalExt}`;
-    outputFileName = newFileName;
-    cb(null, newFileName);
-  },
-});
-const upload_model = multer({ storage: storage_model });
-// 上傳圖片
-router.post("/image/upload", upload_image.single("image"), async (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({
-      success: false,
-      message: "No file uploaded",
-    });
-  }
-
-  try {
-    let tk = await gettk({ baseURL: baseURL.to_db_api });
-    let send2db = {
-      image_url: outputFileName, // 檔案名稱
-      name: req.body.name || outputFileName, // 可以由前端傳入的名稱或直接使用檔案名稱
-    };
-
-    const result = await axios({
-      method: "POST",
-      baseURL: baseURL.to_db_api,
-      url: "/db/image/add",
-      data: send2db,
-      headers: {
-        Authorization: `Bearer ${tk.data}`,
-      },
-    });
-
-    res.json({
-      success: true,
-      filename: req.file.filename,
-      originalname: req.file.originalname,
-      filepath: `/media/mediaRepo/img/${req.file.filename}`,
-      outputFileName,
-      dbResponse: result.data,
-    });
-  } catch (err) {
-    // console.error(err);
-    res.status(500).json({
-      success: false,
-      message: "Error uploading image or saving to database",
-      error: err.message,
-    });
-  }
-});
-// 刪除圖片
-router.post("/image/delete", async (req, res) => {
+router.post("/", async function (req, res) {
   let data = req.body;
-
-  if (!data.image_uuid) {
-    return res.status(400).json({
-      success: false,
-      message: "No image_uuid",
-    });
-  }
-
   let tk = await gettk({ baseURL: baseURL.to_db_api });
-
-  await axios({
+  axios({
     method: "POST",
     baseURL: baseURL.to_db_api,
-    url: "/db/image/delete",
-    data: data,
+    url: "/",
+    data,
     headers: {
       Authorization: `Bearer ${tk.data}`,
     },
   })
     .then((result) => {
-      if (result.data.success) {
-        // 構建本地檔案路徑
-        let file = path.join(__dirname, `../media/mediaRepo/${data.image_url}`);
-
-        // 刪除本地檔案
-        fs.unlink(file, (err) => {
-          if (err) {
-            res.send({ success: false, err: "檔案刪除失敗" });
-          } else {
-            res.send({ success: true });
-          }
-        });
-      }
+      res.send(result.data);
     })
     .catch((err) => {
-      res.send({ success: false, err: "Database Error!" });
+      router_catch_send(req, res, err);
     });
-});
-
-// 上傳模型
-router.post("/model/upload", upload_model.single("model"), async (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({
-      success: false,
-      message: "No file uploaded",
-    });
-  }
-
-  try {
-    // 獲取存取資料庫的 Token
-    let tk = await gettk({ baseURL: baseURL.to_db_api });
-    
-    // 定義要傳到資料庫的資料
-    let send2db = {
-      model_url: outputFileName, // 檔案名稱
-      name: req.body.name || outputFileName, // 可以由前端傳入的名稱或直接使用檔案名稱
-    };
-
-    // 使用 Axios 發送資料到資料庫
-    const result = await axios({
-      method: "POST",
-      baseURL: baseURL.to_db_api,
-      url: "/db/model/add", // 注意修改為模型對應的資料庫路徑
-      data: send2db,
-      headers: {
-        Authorization: `Bearer ${tk.data}`,
-      },
-    });
-
-    // 成功上傳並存入資料庫後返回結果
-    res.json({
-      success: true,
-      filename: req.file.filename,
-      originalname: req.file.originalname,
-      filepath: `/media/mediaRepo/models/${req.file.filename}`, // 修改路徑以存放模型檔案
-      outputFileName,
-      dbResponse: result.data,
-    });
-  } catch (err) {
-    // 錯誤處理
-    res.status(500).json({
-      success: false,
-      message: "Error uploading model or saving to database",
-      error: err.message,
-    });
-  }
 });
 function gettk({ type = null, baseURL = "", data = null }) {
   return new Promise((reslove, reject) => {
@@ -185,5 +46,38 @@ function gettk({ type = null, baseURL = "", data = null }) {
         reslove({ success: false, data: null, errorCode: 3 });
       });
   });
+}
+function router_catch_send(req, res, err) {
+  if (err.response) {
+    error_recode(err.response.data);
+    res.status(err.response.status).send({
+      success: false,
+      data: null,
+      error: `${err.response.data}`,
+      errorCode: 2,
+    });
+  } else if (err.request) {
+    // console.log(err.request);
+    error_recode(err.request);
+    res.status(500).send({ success: false, data: null, errorCode: 2 });
+  } else {
+    error_recode(err.message);
+    console.log(err.message);
+    res.status(500).send({
+      success: false,
+      data: null,
+      error: `${err.message}`,
+      errorCode: 2,
+    });
+  }
+  function error_recode(error) {
+    let thisDate = new Date().toLocaleString("zh-TW");
+    let folder = thisDate.split(" ")[0].split("/").join("_");
+    if (!fs.existsSync(`./log/router.error/${folder}`)) {
+      fs.writeFileSync(`./log/router.error/${folder}`, "", (err) => {});
+    }
+    let error_log = `=====${thisDate}=====\n${error}\n`;
+    fs.appendFileSync(`./log/router.error/${folder}`, error_log, (err) => {});
+  }
 }
 module.exports = router;
